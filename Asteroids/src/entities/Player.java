1961d9b44d24bacddd8430101404ec91a3813767
GTSA - Infinity <williamlindahl03@gmail.com>
2025-03-19 13:32:37 +0100

    påbörjade Player

--- Asteroids/src/entities/Player.java
+++ Asteroids/src/entities/Player.java
@@ -1,5 +1,33 @@
 package entities;
 
 public class Player {
+	
+	private double speed = 0.0d;
+	private Position position;
+	
+	
+	public Player() {
+		
+	}
+	
+	public move() {
+		
+	}
+	
+	public setPos() {
+		
+	}
+	
+	public setPosX() {
+			
+	}
+	
+	public setPosY() {
+		
+	}
+	
+	
+	
+	
 
 }
