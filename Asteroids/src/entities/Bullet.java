52b5a52624bdb9192358645973c25473935e9701
GTSA - Infinity <williamlindahl03@gmail.com>
2025-03-19 15:25:52 +0100

    commit

--- Asteroids/src/entities/Bullet.java
+++ Asteroids/src/entities/Bullet.java
@@ -1,5 +1,39 @@
 package entities;
 
+import pos.Force;
+import pos.Position;
+
 public class Bullet {
+	
+	private double speed = 1.0d;
+	private Position position = new Position();
+	private double angle = 0d;
+	private Force force = new Force();
+	
+	public Bullet() {
+		
+	}
+	
+	public void move() {
+		double[] direction = {speed*Math.cos(angle), speed*Math.cos(angle)};
+		force.add(direction);
+		
+		setPosX((int) (position.getX() + force.getX()));
+		setPosY((int) (position.getY() + force.getY()));
+		speed *= 0.95;
+	}
+	
+	public void setPos(Position pos) {
+		position.setX(pos.getX());
+		position.setY(pos.getY());
+	}
+	
+	public void setPosX(int x) {
+		position.setX(x);
+	}
+	
+	public void setPosY(int y) {
+		position.setY(y);
+	}
 
 }
